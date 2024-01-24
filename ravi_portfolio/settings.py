4c84716bb840306835f141eb0a0b51f96d83ff00
Ravi Singh Patel <rprs80115@gmail.com>
2024-01-25 00:22:14 +0530

    SEO optimised

--- ravi_portfolio/settings.py
+++ ravi_portfolio/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'media/static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
